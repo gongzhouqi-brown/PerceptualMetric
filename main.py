3449fa0c15ca226623df2c1f3de61b35c014574f
Zhouqi <zhouqi_gong@brown.edu>
2021-03-09 22:15:17 -0500

    voxelizer

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-import os
-
-from module.Deformer import get_random_deformation
-from pipeline.Pipeline import Pipeline
-
-
 if __name__ == '__main__':
     pass
--- module/deformers/VoxelizeDeformer.py
+++ module/deformers/VoxelizeDeformer.py
@@ -1,16 +1,18 @@
-from copy import deepcopy
 from typing import Dict, Callable, Tuple
 
 import numpy as np
+import trimesh
+import trimesh.voxel.creation
 
 from module.Deformer import Deformer
 
 
-class MarchingCubesDeformer(Deformer):
+class VoxelizeDeformer(Deformer):
     def transform(self, v: np.ndarray, f: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        # TODO: Implement with igl.offset_surface
-        # vertices, triangles = mcubes.marching_cubes_func(v, f, 16)
-        return deepcopy(v), deepcopy(f)
+        mesh = trimesh.Trimesh(v, f, process=False)
+        g = trimesh.voxel.creation.voxelize(mesh, 1)
+        new_mesh = g.as_boxes()
+        return np.array(new_mesh.vertices), np.array(new_mesh.faces)
 
     @staticmethod
     def get_applicable_configs() -> Dict[str, Callable[[float], float]]:
