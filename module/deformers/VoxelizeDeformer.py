f2c63cb17477eb7ad4994179249fccd92f246926
Zhouqi <zhouqi_gong@brown.edu>
2021-03-17 02:32:44 -0400

    edit voxelizer

--- module/deformers/VoxelizeDeformer.py
+++ module/deformers/VoxelizeDeformer.py
@@ -7,13 +7,35 @@ import trimesh.voxel.creation
 from module.Deformer import Deformer
 
 
+NUM_CUBES = "cubes"
+
+
 class VoxelizeDeformer(Deformer):
     def transform(self, v: np.ndarray, f: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        xs = v[:, 0]
+        ys = v[:, 1]
+        zs = v[:, 2]
+        min_x = min(xs)
+        max_x = max(xs)
+        min_y = min(ys)
+        max_y = max(ys)
+        min_z = min(zs)
+        max_z = max(zs)
+
+        x_len = max_x - min_x
+        y_len = max_y - min_y
+        z_len = max_z - min_z
+
+        largest_len = max(x_len, y_len, z_len)
+
+        size = largest_len / self.config[NUM_CUBES]
+
         mesh = trimesh.Trimesh(v, f, process=False)
-        g = trimesh.voxel.creation.voxelize(mesh, 1)
+        g = trimesh.voxel.creation.voxelize(mesh, size)
         new_mesh = g.as_boxes()
         return np.array(new_mesh.vertices), np.array(new_mesh.faces)
 
     @staticmethod
     def get_applicable_configs() -> Dict[str, Callable[[float], float]]:
-        return {}
+        sampler = lambda q: 2 ** np.floor(q * 3 + 5)
+        return {NUM_CUBES: sampler}
