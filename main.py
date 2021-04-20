e20eedc7f6c994fe66268d25ccf45240a8abb2bf
Zhouqi <zhouqi_gong@brown.edu>
2021-04-20 18:09:01 -0400

    basic setting

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import argparse
+
+from log.logger import setup_logger
+
+
+def initialize():
+    parser = argparse.ArgumentParser(description="Perceptual Metric")
+    parser.add_argument("-f", "--filelog",
+                        help="Output all logs to a daily log file instead of console.",
+                        action="store_true")
+    # parser.add_argument("-d", "--data",
+    #                     help="Data file specified for running.",
+    #                     nargs=1)
+    args = vars(parser.parse_args())
+    setup_logger(args["filelog"])
+
+
 if __name__ == '__main__':
-    pass
+    initialize()
