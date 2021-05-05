c8bac936944391750162fcec7e0f62ee96f890e7
Zhouqi <zhouqi_gong@brown.edu>
2021-05-04 20:47:37 -0400

    new SN

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import argparse
 
+from data.ShapeNet import iterate_shape_net
 from log.logger import setup_logger
 from module.Refiner import count_shape_net_components
+import os
+import igl
+import numpy as np
 
 
 def initialize():
@@ -16,6 +20,17 @@ def initialize():
     setup_logger(args["filelog"])
 
 
+def new_shape_net():
+    counter = 0
+    for old_path in iterate_shape_net():
+        new_path = old_path.replace("ShapeNetCore.v2", "new_SN")
+        v, f = igl.read_triangle_mesh(old_path, np.float64)
+        igl.write_triangle_mesh(new_path, v, f)
+        counter += 1
+        print(counter)
+
+
 if __name__ == '__main__':
     initialize()
-    count_shape_net_components()
+    new_shape_net()
+
